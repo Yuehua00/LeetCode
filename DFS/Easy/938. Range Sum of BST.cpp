18390aacdeac38b21cfd3aee97049cbc8eb55228
Slider <113094262+Yuehua00@users.noreply.github.com>
2024-08-01 09:17:43 +0800

    Update 938. Range Sum of BST.cpp

--- DFS/Easy/938. Range Sum of BST.cpp
+++ DFS/Easy/938. Range Sum of BST.cpp
@@ -11,7 +11,6 @@
  */
 class Solution {
 public:
-    int ans = 0;
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(root == NULL) return 0;
         if(root->val >= low && root->val <= high) return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
