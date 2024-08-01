9d623b36b1806803176868377cc9648b9f354e7b
Slider <113094262+Yuehua00@users.noreply.github.com>
2024-08-01 10:05:11 +0800

    Create 2331. Evaluate Boolean Binary Tree.cpp

---
+++ DFS/Easy/2331. Evaluate Boolean Binary Tree.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/evaluate-boolean-binary-tree/?source=submission-noac
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(root->left == nullptr && root->right == nullptr) return root->val;
+        bool left = evaluateTree(root->left);
+        bool right = evaluateTree(root->right);
+        bool newval;
+        if(root->val == 2) newval = left || right;
+        if(root->val == 3) newval = left && right;
+        return newval;
+    }
+};
